985ea195c86e8f424eab3993a15b34e54b918772
maxence ravau <maxenceravau@gmail.com>
2022-11-14 13:19:17 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @question = params[:questions]
+    @answer = params[:action]
+
+    if @question.include?("?")
+      return @answer =  "Silly question, get dressed and go to work!"
+    elsif @question == "I am going to work"
+      return @answer = "Great!"
+    else
+      return @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+
